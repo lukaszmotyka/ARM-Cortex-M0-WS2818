bf907f3b1740e0c92682c497a98a24f8793ba2ba
lukaszmotyka <lukasmotyka94@gmail.com>
2016-12-21 18:10:28 +0100

    Update ws2818.h

--- ws2818.h
+++ ws2818.h
@@ -5,10 +5,10 @@
 const uint32_t Mask[]={1UL<<3};
 static uint8_t liczba_diod=40;
 static uint8_t color;
-static uint8_t kolory[40][3];
+static uint8_t kolory[39][2];
 void LedInit(void);
 void SendReset(void);
 void SendOne(void);
 void SendZero(void);
-void Display(uint8_t digit,uint8_t color[40][3]);
+void Display(uint8_t digit,uint8_t color[39][2]);
 #endif
